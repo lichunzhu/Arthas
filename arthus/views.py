17d68ac95b3ad53e19500cb3c9e49df6a770e3ca
chauncylee <lichunzhu@stu.xjtu.edu.cn>
2018-09-03 00:35:22 +0800

    add css, html files and add py template

--- arthus/views.py
+++ arthus/views.py
@@ -5,8 +5,27 @@
 # IDE     :PyCharm
 
 from arthus import app
+from flask import render_template, redirect
+from models import User, Image
 
 
 @app.route('/')
 def index():
-    return 'Hello'
+    images = Image.query.order_by('id desc').limit(10)
+    return render_template('index.html', images=images)
+
+
+@app.route("/image/<int:image_id>")
+def image(image_id):
+    image = Image.query.get(image_id)
+    if image is None:
+        return redirect('/')
+    return render_template('pageDetail.html', image=image)
+
+
+@app.route("/profile/<int:user_id>")
+def profile(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return redirect('/')
+    return render_template('profile.html', user=user)
